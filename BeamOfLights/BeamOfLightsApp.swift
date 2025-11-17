07a8585b4030938e40b55c4e61cf40df55a5b239
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2025-11-17 19:04:21 +0300

    Initial Commit

---
+++ BeamOfLights/BeamOfLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamOfLightsApp.swift
+//  BeamOfLights
+//
+//  Created by Akinalp Fidan on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BeamOfLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
